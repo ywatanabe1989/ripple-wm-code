dcc9b239d3c83f69f3bf304f626fc4f58af4b0bd
Yusuke Watanabe <wyusuuke@gmail.com>
2024-08-25 08:00:24 +0900

    Error: Connection error.

--- scripts/NT/TDA/n_samples_in_spheres.py
+++ scripts/NT/TDA/n_samples_in_spheres.py
@@ -1,6 +1,6 @@
 #!./env/bin/python3
 # -*- coding: utf-8 -*-
-# Time-stamp: "2024-08-24 18:35:55 (ywatanabe)"
+# Time-stamp: "2024-08-25 08:59:12 (ywatanabe)"
 # /mnt/ssd/ripple-wm-code/scripts/NT/TDA.py
 
 
@@ -73,16 +73,6 @@ def balance_phase(NT):
     return NTs_pp
 
 
-# def slice_NT_by_match(NT, TI, match):
-#     assert match in MATCH_CONDI
-#     indi = {
-#         "All": np.full(len(TI), True),
-#         "Match IN": TI.match == 1,
-#         "Mismatch OUT": TI.match == 2,
-#     }[match]
-#     return NT[indi]
-
-
 # Distances
 def calc_distances(NT, GS, TI):
     # To the shape of ("factor", "phase", ...)
@@ -129,83 +119,6 @@ def calc_distances(NT, GS, TI):
     return df
 
 
-# # N Samples
-# def calc_n_samples_in_spheres(distances, radii):
-
-#     def _calc_n_samples_in_spheres(distances, radius):
-#         return (distances <= radius).sum()
-
-#     # Calculate the values
-#     df_new = pd.DataFrame(
-#         {
-#             f"radius-{rr}": distances.apply(
-#                 lambda x: _calc_n_samples_in_spheres(x, rr)
-#             )
-#             for rr in radii
-#         }
-#     )
-
-#     # To probability
-#     df_new /= np.array(df_new).max(axis=1, keepdims=True)
-
-#     return df_new
-
-
-# def plot_n_samples(NT, GS, TI, ca1, spath_base):
-#     mngs.plt.configure_mpl(plt, verbose=False)
-
-#     fig, axes = mngs.plt.subplots(
-#         nrows=len(PHASES_TO_PLOT), sharex=True, sharey=True
-#     )
-
-#     # Distance
-#     dist_df = calc_distances(NT, GS, TI)
-
-#     # Radii to count
-#     radii = np.linspace(0, dist_df.dist.max(), 100)
-
-#     # Count
-#     dist_df["n_bin"] = 1
-#     df = []
-#     for rr in radii:
-#         r_df = (
-#             dist_df[dist_df["dist"] < rr]
-#             .groupby(["phase_g", "phase_nt", "match"])
-#             .agg({"n_bin": "sum"})
-#             .reset_index()
-#         )
-#         if len(r_df) > 0:
-#             r_df.loc[:, "radius"] = rr
-#         df.append(r_df)
-#     df = pd.concat(df)
-
-#     conditions = {
-#         k: df[k].unique().tolist() for k in ["phase_g", "phase_nt", "match"]
-#     }
-#     conditions = list(mngs.gen.yield_grids(conditions))
-
-#     fig, ax = mngs.plt.subplots()
-#     for cc in conditions:
-#         df_cc = mngs.pd.slice(df, cc)
-#         df_cc = df_cc.sort_values(["radius"])
-#         xx = df_cc.radius
-#         yy = df_cc.n_bin
-#         yy = yy / yy.max() * 100
-#         ax.plot(xx, yy, label=str(cc))
-#         ax.legend()
-#         ax.set_xscale("log")
-#         ax.set_xlim(0.1, 10)
-#         ax.set_ylim(0, 100)
-
-#     fig.supxyt(
-#         "Radius",
-#         "Sample count [%]",
-#         str(ca1),
-#     )
-
-#     return fig
-
-
 def plot_n_samples(NT, GS, TI, ca1, spath_base):
     mngs.plt.configure_mpl(plt, verbose=False)
 
@@ -215,14 +128,16 @@ def plot_n_samples(NT, GS, TI, ca1, spath_base):
     # Radii to count
     radii = np.logspace(np.log10(0.1), np.log10(10), 100)
 
-
     # Count
     dist_df["n_bin"] = 1
-    all_combinations = pd.MultiIndex.from_product([
-        dist_df["phase_g"].unique(),
-        dist_df["phase_nt"].unique(),
-        dist_df["match"].unique()
-    ], names=["phase_g", "phase_nt", "match"])
+    all_combinations = pd.MultiIndex.from_product(
+        [
+            dist_df["phase_g"].unique(),
+            dist_df["phase_nt"].unique(),
+            dist_df["match"].unique(),
+        ],
+        names=["phase_g", "phase_nt", "match"],
+    )
 
     df = []
     for rr in radii:
@@ -237,21 +152,7 @@ def plot_n_samples(NT, GS, TI, ca1, spath_base):
         df.append(r_df)
     df = pd.concat(df, ignore_index=True)
 
-    # # Count
-    # dist_df["n_bin"] = 1
-    # df = []
-    # for rr in radii:
-    #     r_df = (
-    #         dist_df[dist_df["dist"] < rr]
-    #         .groupby(["phase_g", "phase_nt", "match"])
-    #         .agg({"n_bin": "sum"})
-    #         .reset_index()
-    #     )
-    #     if len(r_df) > 0:
-    #         r_df.loc[:, "radius"] = rr
-    #     df.append(r_df)
-    # df = pd.concat(df)
-
+    # Counting
     conditions = {
         k: df[k].unique().tolist() for k in ["phase_g", "phase_nt", "match"]
     }
@@ -271,35 +172,28 @@ def plot_n_samples(NT, GS, TI, ca1, spath_base):
 
     heatmap_data = np.vstack(heatmap_data)
 
-    sorted_indices = natsorted(range(len(condition_labels)), key=lambda i: condition_labels[i])
+    sorted_indices = natsorted(
+        range(len(condition_labels)), key=lambda i: condition_labels[i]
+    )
 
     heatmap_data = heatmap_data[sorted_indices]
     condition_labels = np.array(condition_labels)[sorted_indices].tolist()
 
     ax.imshow2d(heatmap_data.T)
 
-    condition_labels = [str(cl).replace(", ", ",\n") for cl in condition_labels]
+    condition_labels = [
+        str(cl).replace(", ", ",\n") for cl in condition_labels
+    ]
     ax.set_ticks(
         yvals=np.arange(len(condition_labels)),
         yticks=np.array(condition_labels),
     )
+    ax.set_xyt("Radius", "Conditions", str(ca1))
 
-    mngs.io.save(fig, "/tmp/tmp.jpg")
+    # Saving
+    mngs.io.save(fig, "./" + mngs.gen.title2path(ca1) + ".jpg", from_cwd=True)
 
-    # im = ax.imshow(heatmap_data, aspect='auto', cmap='viridis', extent=[np.log10(0.1), np.log10(10), 0, 8])
-
-    # ax.set_yticks(np.arange(8) + 0.5)
-    # ax.set_yticklabels(condition_labels)
-    # ax.set_xticks(np.log10([0.1, 1, 10]))
-    # ax.set_xticklabels([0.1, 1, 10])
-
-    # cbar = fig.colorbar(im, ax=ax, label='Sample count [%]')
-
-    # ax.set_xlabel('Radius')
-    # ax.set_ylabel('Conditions')
-    # ax.set_title(str(ca1))
-
-    # return fig
+    return fig
 
 
 def main():
@@ -320,7 +214,6 @@ def main():
 
         # N Samples
         plot_n_samples(NT, GS, TI, ca1, spath_base)
-
 
 
 if __name__ == "__main__":
