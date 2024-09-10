f1cff556261ca582c2d74fea04a715d92da515b0
Yusuke Watanabe <wyusuuke@gmail.com>
2024-09-10 19:19:52 +0900

    .

--- scripts/memory-load/distance_between_gs_plot.py
+++ scripts/memory-load/distance_between_gs_plot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: "2024-09-10 11:45:39 (ywatanabe)"
+# Time-stamp: "2024-09-10 14:13:48 (ywatanabe)"
 # /mnt/ssd/ripple-wm-code/scripts/memory-load/distance_between_gs.py
 
 """This script calculates and analyzes distances between phase states."""
@@ -58,47 +58,47 @@ def calculate_distances(gs_data, config):
     return pd.concat(distances, axis=1)
 
 
-def perform_statistical_tests(merged_df):
-    """Perform statistical tests on the merged data."""
-    phase_combinations = ["FE", "FM", "FR", "EM", "ER", "MR"]
-    results = {}
+# def perform_statistical_tests(merged_df):
+#     """Perform statistical tests on the merged data."""
+#     phase_combinations = ["FE", "FM", "FR", "EM", "ER", "MR"]
+#     results = {}
 
-    np.random.seed(42)
-    for phase in phase_combinations:
-        corr, p_value = stats.pearsonr(merged_df[phase], merged_df["set_size"])
-        sample_size = len(merged_df[phase])
-        effect_size = abs(corr)
+#     np.random.seed(42)
+#     for phase in phase_combinations:
+#         corr, p_value = stats.pearsonr(merged_df[phase], merged_df["set_size"])
+#         sample_size = len(merged_df[phase])
+#         effect_size = abs(corr)
 
-        shuffled_corrs = [
-            stats.pearsonr(
-                merged_df[phase], np.random.permutation(merged_df["set_size"])
-            )[0]
-            for _ in range(1000)
-        ]
-        p_value_surrogate = np.mean(np.abs(shuffled_corrs) >= np.abs(corr))
+#         shuffled_corrs = [
+#             stats.pearsonr(
+#                 merged_df[phase], np.random.permutation(merged_df["set_size"])
+#             )[0]
+#             for _ in range(1000)
+#         ]
+#         p_value_surrogate = np.mean(np.abs(shuffled_corrs) >= np.abs(corr))
 
-        groups = [
-            merged_df[merged_df["set_size"] == ss][phase] for ss in [4, 6, 8]
-        ]
-        h_statistic, p_value_kw = stats.kruskal(*groups)
+#         groups = [
+#             merged_df[merged_df["set_size"] == ss][phase] for ss in [4, 6, 8]
+#         ]
+#         h_statistic, p_value_kw = stats.kruskal(*groups)
 
-        bm_results = []
-        for ii, jj in combinations(range(3), 2):
-            bm_test_output = mngs.stats.brunner_munzel_test(
-                groups[ii], groups[jj]
-            )
-            bm_results.append(
-                (f"{[4,6,8][ii]} vs {[4,6,8][jj]}", bm_test_output)
-            )
+#         bm_results = []
+#         for ii, jj in combinations(range(3), 2):
+#             bm_test_output = mngs.stats.brunner_munzel_test(
+#                 groups[ii], groups[jj]
+#             )
+#             bm_results.append(
+#                 (f"{[4,6,8][ii]} vs {[4,6,8][jj]}", bm_test_output)
+#             )
 
-        results[phase] = {
-            "correlation": (corr, p_value, f"{sample_size:,}", effect_size),
-            "surrogate": p_value_surrogate,
-            "kruskal_wallis": (h_statistic, p_value_kw),
-            "brunner_munzel": bm_results,
-        }
+#         results[phase] = {
+#             "correlation": (corr, p_value, f"{sample_size:,}", effect_size),
+#             "surrogate": p_value_surrogate,
+#             "kruskal_wallis": (h_statistic, p_value_kw),
+#             "brunner_munzel": bm_results,
+#         }
 
-    return results
+#     return results
 
 
 def analyze_mtl_region(mtl_region, use_log, config):
@@ -110,17 +110,23 @@ def analyze_mtl_region(mtl_region, use_log, config):
     if use_log:
         distances_reset = np.log10(distances_reset)
     ti_reset = ti_data.reset_index(drop=True)
+
+    distances_reset = distances_reset.melt().rename(
+        columns={
+            "variable": "phase_combination",
+            "value": "distance",
+        }
+    )
+    repeated_ti = pd.concat([ti_reset] * 6, ignore_index=True)
     merged_df = pd.concat(
-        [distances_reset, ti_reset[["correct", "response_time", "set_size"]]],
+        [
+            distances_reset,
+            repeated_ti[["correct", "response_time", "set_size"]],
+        ],
         axis=1,
     )
 
-    correlations = merged_df[
-        ["FE", "FM", "FR", "EM", "ER", "MR", "correct", "response_time"]
-    ].corrwith(merged_df["set_size"])
-    statistical_results = perform_statistical_tests(merged_df)
-
-    return merged_df, correlations, statistical_results
+    return merged_df
 
 
 def main():
@@ -133,126 +139,137 @@ def main():
         )
 
         for idx, mtl_region in enumerate(mtl_regions):
-            merged_df, correlations, statistical_results = analyze_mtl_region(
-                mtl_region, use_log, CONFIG
+            merged_df = analyze_mtl_region(mtl_region, use_log, CONFIG)
+
+            # Plotting
+            fig, ax = mngs.plt.subplots()
+            ax.sns_boxplot(
+                data=merged_df,
+                x="phase_combination",
+                y="distance",
+                hue="set_size",
+                showfliers=False,
             )
+            title = f"{mtl_region}_{'log' if use_log else 'linear'}"
+            ax.set_xyt("Phase Combination", "Distance [a.u.]", title)
+            mngs.io.save(fig, f"box_{title}.jpg")
 
-            ax = axes[idx] if len(mtl_regions) > 1 else axes
-            vlim = 0.275
-            ax.sns_heatmap(
-                correlations.to_frame().T,
-                cmap="coolwarm",
-                center=0,
-                fmt=".3f",
-                vmax=vlim,
-                vmin=-vlim,
-            )
+    #         ax = axes[idx] if len(mtl_regions) > 1 else axes
+    #         vlim = 0.275
+    #         ax.sns_heatmap(
+    #             correlations.to_frame().T,
+    #             cmap="coolwarm",
+    #             center=0,
+    #             fmt=".3f",
+    #             vmax=vlim,
+    #             vmin=-vlim,
+    #         )
 
-            for ii, col in enumerate(
-                [
-                    "FE",
-                    "FM",
-                    "FR",
-                    "EM",
-                    "ER",
-                    "MR",
-                    "correct",
-                    "response_time",
-                ]
-            ):
-                xx = ii + 0.5
-                yy = 1.0
-                dyy = 0.2
-                rotation = 20
-                corr = correlations[col]
+    #         for ii, col in enumerate(
+    #             [
+    #                 "FE",
+    #                 "FM",
+    #                 "FR",
+    #                 "EM",
+    #                 "ER",
+    #                 "MR",
+    #                 "correct",
+    #                 "response_time",
+    #             ]
+    #         ):
+    #             xx = ii + 0.5
+    #             yy = 1.0
+    #             dyy = 0.2
+    #             rotation = 20
+    #             corr = correlations[col]
 
-                ax.text(
-                    xx,
-                    yy - dyy,
-                    f"Corr: {corr:.3f}",
-                    ha="center",
-                    va="center",
-                    fontsize=8,
-                    rotation=rotation,
-                    style="italic",
-                )
+    #             ax.text(
+    #                 xx,
+    #                 yy - dyy,
+    #                 f"Corr: {corr:.3f}",
+    #                 ha="center",
+    #                 va="center",
+    #                 fontsize=8,
+    #                 rotation=rotation,
+    #                 style="italic",
+    #             )
 
-                if col in statistical_results:
-                    corr, p_value, sample_size, effect_size = (
-                        statistical_results[col]["correlation"]
-                    )
-                    corr_stars = mngs.stats.p2stars(p_value)
-                    ax.text(
-                        xx,
-                        yy - dyy * 2,
-                        f"Corr p: {p_value:.3f} {corr_stars}\n(n={sample_size}, eff={effect_size:.3f})",
-                        ha="center",
-                        va="center",
-                        fontsize=8,
-                        rotation=rotation,
-                        style="italic",
-                    )
+    #             if col in statistical_results:
+    #                 corr, p_value, sample_size, effect_size = (
+    #                     statistical_results[col]["correlation"]
+    #                 )
+    #                 corr_stars = mngs.stats.p2stars(p_value)
+    #                 ax.text(
+    #                     xx,
+    #                     yy - dyy * 2,
+    #                     f"Corr p: {p_value:.3f} {corr_stars}\n(n={sample_size}, eff={effect_size:.3f})",
+    #                     ha="center",
+    #                     va="center",
+    #                     fontsize=8,
+    #                     rotation=rotation,
+    #                     style="italic",
+    #                 )
 
-                    surr_stars = mngs.stats.p2stars(
-                        statistical_results[col]["surrogate"]
-                    )
-                    ax.text(
-                        xx,
-                        yy - dyy * 3,
-                        f"Surr p: {statistical_results[col]['surrogate']:.3f} {surr_stars}",
-                        ha="center",
-                        va="center",
-                        fontsize=8,
-                        rotation=rotation,
-                        style="italic",
-                    )
+    #                 surr_stars = mngs.stats.p2stars(
+    #                     statistical_results[col]["surrogate"]
+    #                 )
+    #                 ax.text(
+    #                     xx,
+    #                     yy - dyy * 3,
+    #                     f"Surr p: {statistical_results[col]['surrogate']:.3f} {surr_stars}",
+    #                     ha="center",
+    #                     va="center",
+    #                     fontsize=8,
+    #                     rotation=rotation,
+    #                     style="italic",
+    #                 )
 
-                    kw_stars = mngs.stats.p2stars(
-                        statistical_results[col]["kruskal_wallis"][1]
-                    )
-                    ax.text(
-                        xx,
-                        yy - dyy * 4,
-                        f"KW p: {statistical_results[col]['kruskal_wallis'][1]:.3f} {kw_stars}",
-                        ha="center",
-                        va="center",
-                        fontsize=8,
-                        rotation=rotation,
-                        style="italic",
-                    )
+    #                 kw_stars = mngs.stats.p2stars(
+    #                     statistical_results[col]["kruskal_wallis"][1]
+    #                 )
+    #                 ax.text(
+    #                     xx,
+    #                     yy - dyy * 4,
+    #                     f"KW p: {statistical_results[col]['kruskal_wallis'][1]:.3f} {kw_stars}",
+    #                     ha="center",
+    #                     va="center",
+    #                     fontsize=8,
+    #                     rotation=rotation,
+    #                     style="italic",
+    #                 )
 
-            ax.set_title(
-                f"{mtl_region} ({'Log' if use_log else 'Linear'} scale)"
-            )
+    #         ax.set_title(
+    #             f"{mtl_region} ({'Log' if use_log else 'Linear'} scale)"
+    #         )
 
-            ax.set_ylim(-1, 1)
+    #         ax.set_ylim(-1, 1)
 
-            for phase, results in statistical_results.items():
-                all_results.append(
-                    {
-                        "MTL": mtl_region,
-                        "Scale": "Log" if use_log else "Linear",
-                        "Phase": phase,
-                        "Correlation": f"{results['correlation'][0]:.3f}",
-                        "Correlation_p": f"{results['correlation'][1]:.3f}",
-                        "Sample_size": f"{results['correlation'][2]}",
-                        "Effect_size": f"{results['correlation'][3]:.3f}",
-                        "Surrogate_p": f"{results['surrogate']:.3f}",
-                        "KW_H": f"{results['kruskal_wallis'][0]:.3f}",
-                        "KW_p": f"{results['kruskal_wallis'][1]:.3f}",
-                        "BM_4v6_w": f"{results['brunner_munzel'][0][1]['w_statistic']:.3f}",
-                        "BM_4v6_p": f"{results['brunner_munzel'][0][1]['p_value']:.3f}",
-                        "BM_4v8_w": f"{results['brunner_munzel'][1][1]['w_statistic']:.3f}",
-                        "BM_4v8_p": f"{results['brunner_munzel'][1][1]['p_value']:.3f}",
-                        "BM_6v8_w": f"{results['brunner_munzel'][2][1]['w_statistic']:.3f}",
-                        "BM_6v8_p": f"{results['brunner_munzel'][2][1]['p_value']:.3f}",
-                    }
-                )
+    #         for phase, results in statistical_results.items():
+    #             all_results.append(
+    #                 {
+    #                     "MTL": mtl_region,
+    #                     "Scale": "Log" if use_log else "Linear",
+    #                     "Phase": phase,
+    #                     "Correlation": f"{results['correlation'][0]:.3f}",
+    #                     "Correlation_p": f"{results['correlation'][1]:.3f}",
+    #                     "Sample_size": f"{results['correlation'][2]}",
+    #                     "Effect_size": f"{results['correlation'][3]:.3f}",
+    #                     "Surrogate_p": f"{results['surrogate']:.3f}",
+    #                     "KW_H": f"{results['kruskal_wallis'][0]:.3f}",
+    #                     "KW_p": f"{results['kruskal_wallis'][1]:.3f}",
+    #                     "BM_4v6_w": f"{results['brunner_munzel'][0][1]['w_statistic']:.3f}",
+    #                     "BM_4v6_p": f"{results['brunner_munzel'][0][1]['p_value']:.3f}",
+    #                     "BM_4v8_w": f"{results['brunner_munzel'][1][1]['w_statistic']:.3f}",
+    #                     "BM_4v8_p": f"{results['brunner_munzel'][1][1]['p_value']:.3f}",
+    #                     "BM_6v8_w": f"{results['brunner_munzel'][2][1]['w_statistic']:.3f}",
+    #                     "BM_6v8_p": f"{results['brunner_munzel'][2][1]['p_value']:.3f}",
+    #                 }
+    #             )
 
-        plt.tight_layout()
-        mngs.io.save(fig, f"summary_plot_{'log' if use_log else 'linear'}.jpg")
+    #     plt.tight_layout()
+    #     mngs.io.save(fig, f"summary_plot_{'log' if use_log else 'linear'}.jpg")
 
-    mngs.io.save(pd.DataFrame(all_results), "summary_stats.csv")
+    # mngs.io.save(pd.DataFrame(all_results), "summary_stats.csv")
 
 
 if __name__ == "__main__":
