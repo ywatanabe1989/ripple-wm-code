17f0992029ce3c6bd923ddccf723dc6f6a886ccc
Yusuke Watanabe <wyusuuke@gmail.com>
2024-09-15 18:51:32 +0900

    .

--- scripts/ripple/NT/direction/radian_kde_polar_plot.py
+++ scripts/ripple/NT/direction/radian_kde_polar_plot.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: "2024-09-15 19:14:55 (ywatanabe)"
+# Time-stamp: "2024-09-15 19:36:26 (ywatanabe)"
 # /mnt/ssd/ripple-wm-code/scripts/ripple/NT/distance_from_O.py
 
 """
-This version creates a 3x3 grid of plots:
+This script creates a 3x3 grid of plots:
 - Columns: All data, Match IN, Mismatch OUT
 - Rows:
 1. eSWR+*rSWR+, eSWR+*vER, rSWR+*vER
@@ -12,7 +12,6 @@ This version creates a 3x3 grid of plots:
 3. Differences
 """
 
-"""Imports"""
 import os
 import random
 import sys
@@ -22,10 +21,10 @@ import mngs
 import numpy as np
 import pandas as pd
 from scipy.spatial.distance import cdist
+from scipy.stats import gaussian_kde
 from scripts import utils
 from scripts.ripple.NT.distance.from_O_lineplot import calc_dist_by_condi
 
-"""Functions & Classes"""
 XLIM = (0, np.pi)
 COMPARISONS = [
     "eSWR_vs_eSWR",
@@ -36,7 +35,7 @@ COMPARISONS = [
 ]
 
 
-def process_data(swr_all, match, set_size, SWR_direction_def):
+def process_data(swr_all, match, set_size, swr_direction_def):
     radian = {}
     for ca1 in CONFIG.ROI.CA1:
         swr_all["sub"] = swr_all["subject"]
@@ -49,51 +48,65 @@ def process_data(swr_all, match, set_size, SWR_direction_def):
             df = df[df.set_size == set_size]
 
         for swr_type in CONFIG.RIPPLE.TYPES:
-            df_E = df[(df["swr_type"] == swr_type) & (df.phase == "Encoding")]
-            df_R = df[(df["swr_type"] == swr_type) & (df.phase == "Retrieval")]
+            df_encoding = df[
+                (df["swr_type"] == swr_type) & (df.phase == "Encoding")
+            ]
+            df_retrieval = df[
+                (df["swr_type"] == swr_type) & (df.phase == "Retrieval")
+            ]
 
-            if df_E.empty or df_R.empty:
+            if df_encoding.empty or df_retrieval.empty:
                 radian[f"{ca1.values()}_{swr_type}_eSWR_vs_rSWR"] = [np.nan]
                 radian[f"{ca1.values()}_{swr_type}_eSWR_vs_vER"] = [np.nan]
                 radian[f"{ca1.values()}_{swr_type}_rSWR_vs_vER"] = [np.nan]
-
             else:
-                v1_SWR = np.vstack(df_E[f"vSWR_def{SWR_direction_def}"])
-                v2_SWR = np.vstack(df_R[f"vSWR_def{SWR_direction_def}"])
-                v_ER = np.vstack(df_R["vER"])
+                v1_swr = np.vstack(df_encoding[f"vSWR_def{swr_direction_def}"])
+                v2_swr = np.vstack(
+                    df_retrieval[f"vSWR_def{swr_direction_def}"]
+                )
+                v_er = np.vstack(df_retrieval["vER"])
 
                 radian[f"{ca1.values()}_{swr_type}_eSWR_vs_rSWR"] = np.arccos(
-                    1 - cdist(v1_SWR, v2_SWR, metric="cosine").reshape(-1)
+                    1 - cdist(v1_swr, v2_swr, metric="cosine").reshape(-1)
                 )
                 radian[f"{ca1.values()}_{swr_type}_eSWR_vs_vER"] = np.arccos(
-                    1 - cdist(v1_SWR, v_ER, metric="cosine")[:, 0]
+                    1 - cdist(v1_swr, v_er, metric="cosine")[:, 0]
                 )
                 radian[f"{ca1.values()}_{swr_type}_rSWR_vs_vER"] = np.arccos(
-                    1 - cdist(v2_SWR, v_ER, metric="cosine")[:, 0]
+                    1 - cdist(v2_swr, v_er, metric="cosine")[:, 0]
                 )
                 radian[f"{ca1.values()}_{swr_type}_eSWR_vs_eSWR"] = np.arccos(
-                    1 - cdist(v1_SWR, v1_SWR, metric="cosine").reshape(-1)
+                    1 - cdist(v1_swr, v1_swr, metric="cosine").reshape(-1)
                 )
                 radian[f"{ca1.values()}_{swr_type}_rSWR_vs_rSWR"] = np.arccos(
-                    1 - cdist(v2_SWR, v2_SWR, metric="cosine").reshape(-1)
+                    1 - cdist(v2_swr, v2_swr, metric="cosine").reshape(-1)
                 )
 
-    df = mngs.pd.force_df(radian)
-    return df
+    return mngs.pd.force_df(radian)
 
 
-def main(SWR_direction_def=1, set_size=4):
+def main(swr_direction_def=1, set_size=4):
     swr_p_all, swr_m_all = utils.load_ripples(with_NT=True)
     swr_p_all["swr_type"] = "SWR+"
     swr_m_all["swr_type"] = "SWR-"
     swr_all = pd.concat([swr_p_all, swr_m_all])
 
-    df_all = process_data(swr_all, "all", set_size, SWR_direction_def)
-    df_in = process_data(swr_all, 1, set_size, SWR_direction_def)
-    df_out = process_data(swr_all, 2, set_size, SWR_direction_def)
+    df_all = process_data(swr_all, "all", set_size, swr_direction_def)
+    df_in = process_data(swr_all, 1, set_size, swr_direction_def)
+    df_out = process_data(swr_all, 2, set_size, swr_direction_def)
+
+    _fig, _axes = mngs.plt.subplots(
+        ncols=3,
+        nrows=3,
+    )
 
     fig, axes = mngs.plt.subplots(
-        ncols=3, nrows=3, figsize=(15, 15), sharex=True, sharey=True
+        ncols=3,
+        nrows=3,
+        figsize=(15, 15),
+        subplot_kw=dict(projection="polar"),
+        sharex=False,
+        sharey=False,
     )
 
     for col, (df, title) in enumerate(
@@ -101,8 +114,8 @@ def main(SWR_direction_def=1, set_size=4):
     ):
         for i_swr_type, swr_type in enumerate(CONFIG.RIPPLE.TYPES):
             ax = axes[i_swr_type, col]
+            _ax = _axes[i_swr_type, col]
             ax.set_title(f"{title} - {swr_type}")
-            ax.set_xlim(*XLIM)
 
             for comparison in COMPARISONS:
                 data = df[
@@ -114,17 +127,33 @@ def main(SWR_direction_def=1, set_size=4):
                 ]
                 data = data.values.flatten()
                 data = data[~np.isnan(data)]
-                ax.kde(
+
+                # Plot to _ax with kde() to store data
+                id = f"{title}-{comparison}-{set_size}-{swr_type}"
+                _ax.kde(
                     data,
+                    id=id,
+                )
+                id = id.replace("+", "\+").replace("-", "\-")
+                dd = _ax.to_sigma()
+                x = np.array(
+                    dd[mngs.gen.search(id + "_kde_x", dd.columns)[1]]
+                ).reshape(-1)
+                kde = np.array(
+                    dd[mngs.gen.search(id + "_kde_kde", dd.columns)[1]]
+                ).reshape(-1)
+
+                # Plot to ax for polar representation
+                ax.plot(
+                    x,
+                    kde,
                     label=f"{comparison}",
-                    id=f"{title}-{comparison}-{set_size}-{swr_type}",
+                    id=id,
                     color=CC[CONFIG.COLORS[comparison]],
-                    xlim=XLIM,
                 )
             ax.legend()
 
-    # Difference plot
-    plotted = axes.to_sigma()
+    _plotted = _axes.to_sigma()
 
     for col, (df, title) in enumerate(
         zip([df_all, df_in, df_out], ["All", "Match IN", "Mismatch OUT"])
@@ -133,46 +162,57 @@ def main(SWR_direction_def=1, set_size=4):
         ax.set_title(f"{title} - Diff (SWR+ - SWR-)")
 
         for comparison in COMPARISONS:
-            x_p = plotted[
+            x_p = _plotted[
                 mngs.gen.search(
                     rf"{title}-{comparison}-{set_size}-SWR\+_kde_x",
-                    plotted.columns,
+                    _plotted.columns,
                 )[1]
             ]
-            kde_p = plotted[
+            kde_p = _plotted[
                 mngs.gen.search(
                     rf"{title}-{comparison}-{set_size}-SWR\+_kde_kde",
-                    plotted.columns,
+                    _plotted.columns,
                 )[1]
             ]
-            x_m = plotted[
+            x_m = _plotted[
                 mngs.gen.search(
                     rf"{title}-{comparison}-{set_size}-SWR\-_kde_x",
-                    plotted.columns,
+                    _plotted.columns,
                 )[1]
             ]
-            kde_m = plotted[
+            kde_m = _plotted[
                 mngs.gen.search(
                     rf"{title}-{comparison}-{set_size}-SWR\-_kde_kde",
-                    plotted.columns,
+                    _plotted.columns,
                 )[1]
             ]
             kde_diff = np.array(kde_p) - np.array(kde_m)
+
             ax.plot(
                 x_p,
                 kde_diff,
                 label=f"{comparison}",
-                id=f"{title}-{comparison}-{set_size}-diff (SWR+ - SWR-)",
                 color=CC[CONFIG.COLORS[f"{comparison}"]],
             )
 
-        ax.axhline(y=0, color="gray", linestyle="--", linewidth=0.8, alpha=0.5)
-        ax.set_xlim(*XLIM)
         ax.legend()
 
-    spath = f"./kde_vSWR_def{SWR_direction_def}/set_size_{set_size}.jpg"
+    # mngs.plt.ax.sharey(*axes.flat[:-3])
+    # mngs.plt.ax.sharey(*axes.flat[-3:])
+
+    for ax in axes.flat:
+        ax.set_ylim(0, ax.get_ylim()[1])
+
+    for ax in axes.flat[-2:]:
+        ax.set_ylim(-10e-4, 10e-4)
+
+    # # Set y-limit for difference plots
+    # for ax in axes.flat[-2:]:
+    #     ax.set_ylim(-1, 1)
+
+    spath = f"./kde_vSWR_def{swr_direction_def}/set_size_{set_size}.jpg"
     fig.supxyt(
-        f"Radian (vSWR def. {SWR_direction_def}) (similar <---> dissimilar)",
+        f"Radian (vSWR def. {swr_direction_def}) (similar <---> dissimilar)",
         "KDE density",
         spath,
     )
@@ -190,16 +230,11 @@ if __name__ == "__main__":
         np=np,
         torch=None,
         fig_scale=2,
-        # font_size_base=10,
-        # font_size_title=10,
-        # font_size_axis_label=9,
-        # font_size_tick_label=9,
-        # font_size_legend=8,
         line_width=3,
     )
     for set_size in ["all"] + CONFIG.SET_SIZES:
-        main(SWR_direction_def=1, set_size=set_size)
-        main(SWR_direction_def=2, set_size=set_size)
+        main(swr_direction_def=1, set_size=set_size)
+        main(swr_direction_def=2, set_size=set_size)
     mngs.gen.close(CONFIG, verbose=False, notify=False)
 
 # EOF
