4528d1d5aed70699b1f3efcf024a6b921767c68a
Yusuke Watanabe <wyusuuke@gmail.com>
2024-09-15 19:15:43 +0900

    .

--- scripts/ripple/stats/count.py
+++ scripts/ripple/stats/count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: "2024-09-14 19:39:11 (ywatanabe)"
+# Time-stamp: "2024-09-15 20:14:59 (ywatanabe)"
 # /mnt/ssd/ripple-wm-code/scripts/ripple/stats/time_course.py
 
 """This script does XYZ."""
@@ -11,23 +11,20 @@ import sys
 import matplotlib.pyplot as plt
 import mngs
 import numpy as np
+import pandas as pd
 from scripts.utils import load_ripples
 
 """Functions & Classes"""
 
 
 def main():
-    pp, mm = load_ripples()
-
-    xx = pp
-
-    xx = xx.set_index(["subject", "session", "roi"])
+    pp, mm = load_ripples(with_NT=True)
+    xx = pp.reset_index().set_index(
+        ["subject", "session", "roi", "trial_number"]
+    )
     xx["n_ripple"] = 1
     ns = xx.groupby(["subject", "session", "roi"]).agg({"n_ripple": ["sum"]})
-    ns = xx.groupby(["subject", "roi"]).agg({"n_ripple": ["sum"]})
-
     print(ns)
-
     mngs.io.save(ns, "ns.csv")
 
 
