0da6b5a1fa859089f21fe222718201185c177574
Yusuke Watanabe <wyusuuke@gmail.com>
2024-10-07 18:44:46 +0900

    .

--- scripts/NT/distance/between_gs/set_size_dependency/plot_box.py
+++ scripts/NT/distance/between_gs/set_size_dependency/plot_box.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: "2024-10-07 19:45:03 (ywatanabe)"
+# Time-stamp: "2024-10-07 20:43:40 (ywatanabe)"
 # /mnt/ssd/ripple-wm-code/scripts/NT/distance/from_O_of_MTL_regions.py
 
 """This script does XYZ."""
@@ -25,13 +25,18 @@ from copy import deepcopy
 CONFIG = mngs.gen.load_configs()
 
 """Functions & Classes"""
+
+
 def roi2mtl(roi):
     for mtl, subregions in CONFIG.ROI.MTL.items():
         if roi in subregions:
             return mtl
 
+
 def load_dists_match_set_size():
-    LPATHS_DIST_BETWEEN_GS_MATCH_SET_SIZE = mngs.gen.glob(CONFIG.PATH.NT_DIST_BETWEEN_GS_MATCH_SET_SIZE)
+    LPATHS_DIST_BETWEEN_GS_MATCH_SET_SIZE = mngs.gen.glob(
+        CONFIG.PATH.NT_DIST_BETWEEN_GS_MATCH_SET_SIZE
+    )
     dists = []
     for lpath_dists in LPATHS_DIST_BETWEEN_GS_MATCH_SET_SIZE:
         parsed = utils.parse_lpath(lpath_dists)
@@ -39,45 +44,25 @@ def load_dists_match_set_size():
             continue
         _dists = mngs.io.load(lpath_dists)
         _dists["MTL"] = roi2mtl(parsed["roi"])
-        for k,v in parsed.items():
+        for k, v in parsed.items():
             _dists[k] = v
         dists.append(_dists)
     dists = pd.concat(dists)
     return dists
 
-# def load_dists_match_all_set_size():
-#     LPATHS_DIST_BETWEEN_GS_MATCH_SET_SIZE = mngs.gen.glob(CONFIG.PATH.NT_DIST_BETWEEN_GS_MATCH_SET_SIZE)
-#     dists = []
-#     for lpath_dists in LPATHS_DIST_BETWEEN_GS_MATCH_SET_SIZE:
-#         parsed = utils.parse_lpath(lpath_dists)
-#         if parsed["session"] not in CONFIG.SESSION.FIRST_TWO:
-#             continue
-#         _dists = mngs.io.load(lpath_dists)
-#         _dists["MTL"] = roi2mtl(parsed["roi"])
-#         for k,v in parsed.items():
-#             _dists[k] = v
-#         dists.append(_dists)
-#     dists = pd.concat(dists)
-#     return dists
-
-def add_match_all(df):
-    df_match_all = deepcopy(df)
-    df_match_all["match"] = -1
-    df = pd.concat([df, df_match_all])
-    match_mapper = deepcopy(CONFIG.MATCHES_STR)
-    match_mapper["-1"] = match_mapper.pop("all")
-    df["match"] = df["match"].astype(str).replace(match_mapper)
-    return df
 
 def main():
     # Loading
     df = load_dists_match_set_size()
-    df = add_match_all(df)
+    df.match = df.match.replace(CONFIG.MATCHES_STR)
 
     # Plotting
-    fig, axes = mngs.plt.subplots(ncols=len(CONFIG.MATCHES_STR), nrows=len(CONFIG.ROI.MTL.keys()))
+    fig, axes = mngs.plt.subplots(
+        ncols=len(CONFIG.MATCHES_STR), nrows=len(CONFIG.ROI.MTL.keys())
+    )
+    sorted_CONFIG_STR = list(CONFIG.MATCHES_STR.values())[-1:] + list(CONFIG.MATCHES_STR.values())[:-1]
     for i_mtl, mtl in enumerate(CONFIG.ROI.MTL.keys()):
-        for i_match, match_str in enumerate(CONFIG.MATCHES_STR.values()):
+        for i_match, match_str in enumerate(sorted_CONFIG_STR):
             ax = axes[i_match, i_mtl]
 
             indi_MTL = df.MTL == mtl
@@ -90,10 +75,12 @@ def main():
                 hue="set_size",
                 showfliers=False,
                 id=mtl,
-                )
+            )
 
-            ax.set_xyt(None, None, f"{mtl}-{match_str}")
+            ax.set_xyt("Phase combination", "Distance [a.u.]", f"{mtl}-{match_str}")
+            ax.get_legend().remove()
 
+    fig.tight_layout()
     mngs.io.save(fig, "box.jpg")
 
 
@@ -103,6 +90,12 @@ if __name__ == "__main__":
         plt,
         verbose=False,
         agg=True,
+        fig_scale=4,
+        font_size_base=6,
+        font_size_title=6,
+        font_size_axis_label=6,
+        font_size_tick_label=6,
+        font_size_legend=6,
     )
     main()
     mngs.gen.close(CONFIG, verbose=False, notify=False)
